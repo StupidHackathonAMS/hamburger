2d524457707686e971672f9151ff7e3f5b981010
Lars Bohm <larsbohm@gmail.com>
2016-08-13 17:45:46 +0200

    add title randomizer

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
+var random_titles1 = ["hot", "steamy", "cold", "icy", "greasy", "big fat", "extravagant", "flamboyant", "diet", "dirty", "exceptional"]
+var random_titles2 = ["hamburger", "bal", "patat", "bamihap", "spaghetti", "milkshake", "frikadel", "tosti", "hotdog", "icecream", "boerenkool", "andijvie", "spinazie", "rookworst", "risotto", "curry", "pudding", "rijst"]
+
+
+var random_prefix = random_titles1[Math.floor(Math.random()*random_titles1.length)];
+var random_suffix = random_titles2[Math.floor(Math.random()*random_titles2.length)];
+
 chrome.bookmarks.onCreated.addListener(function(id, bookmark) {        
-  var new_title = "random title";
+  var new_title = random_prefix + random_suffix;
   chrome.bookmarks.update(id, {title : new_title});
 });
+
+
 
 // CREATE A BOOKMARK //////////////////////////////////////
 // 
