06a78b659d0696c48cdc42c116ce8771b9d71b48
Aafke Smal <aafke.smal@gmail.com>
2016-08-13 16:50:21 +0200

    testing stash

--- background.js
+++ background.js
@@ -1,9 +1,17 @@
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.bookmarks.create({
-    title: "Epic",
-    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-  });
+chrome.bookmarks.onCreated.addListener(function(id, bookmark){
+  console.log(bookmark.title)
+  // chrome.bookmarks.create({
+  //   title: "FUNFUNFUN",
+  //   url: "https://www.youtube.com/watch?v=IO9XlQrEt2Y&ab_channel=RickAstleyVEVO"
+  // });
 });
+
+// chrome.browserAction.onClicked.addListener(function() {
+//   chrome.bookmarks.create({
+//     title: "Epic",
+//     url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+//   });
+// });
 
 
 chrome.tabs.executeScript({
